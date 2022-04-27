a310bef650eeaf514867dc0ab076ae9cc86c59dc
Chatman R. Jr <crjr.code@protonmail.com>
2022-04-27 16:29:49 -0400

    Update the workspace to more clearly illustrate concepts.

--- config/color.js
+++ config/color.js
@@ -5,49 +5,36 @@ import {
 
 import {
   accessibility,
-  convert,
-  harmony,
-  output,
   palette,
   tokens,
 } from "https://cdn.jsdelivr.net/gh/quarksuite/core@2.0.0-20/color.min.js";
-
-const scheme = preset(harmony, { configuration: "triadic" });
 
 const uiA11y = preset(accessibility, { large: true });
 const headingA11y = preset(accessibility, {});
 const textA11y = preset(accessibility, { rating: "AAA" });
 
-const material = preset(palette, { contrast: 90, accented: true });
+const material = preset(palette, { contrast: 95, accented: true });
 const materialDark = preset(palette, {
-  contrast: 95,
+  contrast: 90,
   accented: true,
   dark: true,
 });
 
 function paletteFactory(swatch) {
-  const generator = (data) =>
-    data.map((color) => [material(color), materialDark(color)]);
+  const generator = (color) => [material(color), materialDark(color)];
 
-  const assembler = (data) =>
-    data.reduce(
-      (acc, [light, dark], pos) => ({
-        ...acc,
-        [["main", "accent", "highlight", "spot"][pos]]: {
-          ui: pipeline(light, uiA11y, tokens),
-          heading: pipeline(light, headingA11y, tokens),
-          text: pipeline(light, textA11y, tokens),
-          dark: {
-            ui: pipeline(dark, uiA11y, tokens),
-            heading: pipeline(dark, headingA11y, tokens),
-            text: pipeline(dark, textA11y, tokens),
-          },
-        },
-      }),
-      {},
-    );
+  const assembler = ([light, dark]) => ({
+    ui: pipeline(light, uiA11y, tokens),
+    heading: pipeline(light, headingA11y, tokens),
+    text: pipeline(light, textA11y, tokens),
+    dark: {
+      ui: pipeline(dark, uiA11y, tokens),
+      heading: pipeline(dark, headingA11y, tokens),
+      text: pipeline(dark, textA11y, tokens),
+    },
+  });
 
-  return pipeline(swatch, scheme, generator, assembler);
+  return pipeline(swatch, generator, assembler);
 }
 
 export default paletteFactory;
--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 import color from "./config/color.js";
 
 // Functional DOM helpers
-import { element, set } from "./dom.js";
+import { copy, element, set } from "./dom.js";
 
-const dict = {
-  color: color("#7ea"),
-};
+copy(
+  element("seasons"),
+  element("spring"),
+  element("summer"),
+  element("fall"),
+  element("winter"),
+);
 
-set({ dictionary: dict.color }, element("sample"));
+set(
+  {
+    dictionary: {
+      color: {
+        spring: color(element("spring").color),
+        summer: color(element("summer").color),
+        fall: color(element("fall").color),
+        winter: color(element("winter").color),
+      },
+    },
+  },
+  element("color"),
+);
