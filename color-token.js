44323f525cecc6421471b7e595717a4094c16711
Chatman R. Jr <crjr.code@protonmail.com>
2022-06-18 12:01:24 -0400

    Remove referencing and assignment behavior from <color-token>

--- color-token.js
+++ color-token.js
@@ -1,37 +1,17 @@
-import { convert } from "https://cdn.jsdelivr.net/gh/quarksuite/core@2.0.0-26/color.js";
+import { convert } from "https://cdn.jsdelivr.net/gh/quarksuite/core@2.0.2/color.js";
 
 export class ColorToken extends HTMLElement {
   constructor() {
     super();
 
-    this.as = this.as;
-    this.from = this.from;
     this.color = this.color;
     this.format = this.format;
 
     this.shadow = this.attachShadow({ mode: "open" });
   }
 
-  #as = "unassigned";
-  #from = "";
   #color = "#808080";
   #format = "hex rgb hsl";
-
-  set as(value) {
-    this.reflect("as", value);
-  }
-
-  get as() {
-    return this.getAttribute("as");
-  }
-
-  set from(value) {
-    this.reflect("from", value);
-  }
-
-  get from() {
-    return this.getAttribute("from");
-  }
 
   set color(value) {
     this.reflect("color", value);
@@ -58,12 +38,11 @@ export class ColorToken extends HTMLElement {
   }
 
   static get observedAttributes() {
-    return ["as", "from", "color", "format"];
+    return ["color", "format"];
   }
 
   // Formatting
   formats() {
-    const as = this.as || this.#as;
     const color = this.color || this.#color;
     const format = this.format || this.#format;
 
@@ -79,45 +58,12 @@ export class ColorToken extends HTMLElement {
       : `<span part="value" style="text-transform: lowercase;">${as} <code part="code">${color}</code></span>`;
   }
 
-  // Assignment
-  assigned() {
-    const as = this.as || this.#as;
-    const color = this.color || this.#color;
-    const format = this.format || this.#format;
-
-    this.token = { [as]: color };
-
-    return format !== "none" ? `<span part="label">${as}</span>` : ``;
-  }
-
-  // Referencing
-  referenced() {
-    // Reference is a color scale value
-    if (this.from.split(".").length > 1) {
-      const [scale, value] = this.from.split(".");
-      const target = document.querySelector(`[as="${scale}"]`);
-
-      // A scale reference will only ever be a numbered index or direct assignment
-      this.reference = Array.from(target.children)
-        .find((el, i) => Number(value) === i || el.as === value)
-        .getAttribute("color");
-    } else {
-      const target = document.querySelector(`[as="${this.from}"]`);
-
-      // Reference is a direct assignment
-      this.reference = target.getAttribute("color");
-    }
-
-    this.color = this.reference;
-  }
-
   template() {
     const color = this.color || this.#color;
     const tmpl = document.createElement("template");
 
     tmpl.innerHTML = `
 ${this.styles(color)}
-${this.assigned()}
 <div part="swatch"></div>
 <div part="data">
 ${this.formats()}
@@ -141,11 +87,6 @@ ${this.formats()}
 
   :host[hidden] {
     display: none;
-  }
-
-  [part="label"] {
-    display: block;
-    margin-bottom: var(--spacing);
   }
 
   [part="swatch"] {
@@ -176,9 +117,6 @@ ${this.formats()}
   }
 
   connectedCallback() {
-    if (this.from) {
-      this.referenced();
-    }
     this.render();
   }
 }
