ad94dbb90036ed07602c1b1348abd3ef254dcf9e
Chatman R. Jr <crjr.code@protonmail.com>
2022-06-20 12:58:23 -0400

    Refactor <color-token>.

--- color-token.js
+++ color-token.js
@@ -1,5 +1,73 @@
 import { convert } from "https://cdn.jsdelivr.net/gh/quarksuite/core@2.0.2/color.js";
 
+// Formatting
+function formats(color, format) {
+  return format !== "none"
+    ? format
+        .split(" ")
+        .map((format) => {
+          return `<span part="value">${format}: <code part="code ${
+            color === convert(format, color) && `actual`
+          }">${convert(format, color)}</code></span>`;
+        })
+        .join("")
+    : `<span part="value" style="text-transform: lowercase;">${as} <code part="code">${color}</code></span>`;
+}
+
+// Template
+function template(color, format) {
+  const tmpl = document.createElement("template");
+
+  tmpl.innerHTML = `
+${styles(color)}
+<div part="swatch"></div>
+<div part="data">
+${formats(color, format)}
+</div>
+`;
+
+  return tmpl.content.cloneNode(true);
+}
+
+// Styles
+function styles(color) {
+  return `
+<style>
+  :host {
+    display: block;
+
+    --spacing: 1ex;
+    --swatch-size: 10vh;
+    --data-size: 1rem;
+  }
+
+  :host[hidden] {
+    display: none;
+  }
+
+  [part="swatch"] {
+    background: ${convert("hex", color)};
+    min-width: var(--swatch-size);
+    min-height: var(--swatch-size);
+  }
+
+  [part="data"] {
+    font-size: var(--data-size);
+    margin-top: calc(var(--spacing) / 2);
+  }
+
+  [part="value"] {
+    display: block;
+    margin-bottom: calc(var(--spacing) / 4);
+  }
+
+  [part~="actual"] {
+    font-weight: 700;
+  }
+</style>
+`;
+}
+
 export class ColorToken extends HTMLElement {
   constructor() {
     super();
@@ -38,77 +106,9 @@ export class ColorToken extends HTMLElement {
     return ["color", "format"];
   }
 
-  // Formatting
-  formats() {
-    const { color, format } = this;
-
-    return format !== "none"
-      ? format
-        .split(" ")
-        .map((format) => {
-          return `<span part="value">${format}: <code part="code ${
-            color === convert(format, color) && `actual`
-          }">${convert(format, color)}</code></span>`;
-        })
-        .join("")
-      : `<span part="value" style="text-transform: lowercase;">${as} <code part="code">${color}</code></span>`;
-  }
-
-  template() {
-    const { color } = this;
-    const tmpl = document.createElement("template");
-
-    tmpl.innerHTML = `
-${this.styles(color)}
-<div part="swatch"></div>
-<div part="data">
-${this.formats()}
-</div>
-`;
-
-    return tmpl.content.cloneNode(true);
-  }
-
-  styles(color) {
-    return `
-<style>
-  :host {
-    display: block;
-
-    --spacing: 1ex;
-    --swatch-size: 10vh;
-    --data-size: 1rem;
-  }
-
-  :host[hidden] {
-    display: none;
-  }
-
-  [part="swatch"] {
-    background: ${convert("hex", color)};
-    min-width: var(--swatch-size);
-    min-height: var(--swatch-size);
-  }
-
-  [part="data"] {
-    font-size: var(--data-size);
-    margin-top: calc(var(--spacing) / 2);
-  }
-
-  [part="value"] {
-    display: block;
-    margin-bottom: calc(var(--spacing) / 4);
-  }
-
-  [part~="actual"] {
-    font-weight: 700;
-  }
-</style>
-`;
-  }
-
   render() {
-    return this.shadow.append(this.template());
+    const { color, format } = this;
+    return this.shadow.append(template(color, format));
   }
 
   connectedCallback() {
